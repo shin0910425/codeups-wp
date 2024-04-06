fccb38d9f8ec8ca6698e1dcbe8ad39b1f0024f7b
shin0910425 <shin0910425@gmail.com>
2024-04-06 22:21:01 +0900

    sidebar修正

--- WordPressTheme/assets/sidebar.php
+++ WordPressTheme/assets/sidebar.php
@@ -1,4 +1,4 @@
-<aside class="page-blog__sidebar page-blog__sidebar--detail">
+<aside class="page-sidebar page-sidebar--detail">
 
   <?php
   if (is_active_sidebar('sidebar-widgets')) {
@@ -6,9 +6,9 @@
   }
   ?>
 
-  <div class="page-blog__sidebar-items">
-    <div class="page-blog__sidebar-item">
-      <h3 class="page-blog__sidebar-title">人気記事</h3>
+  <div class="page-sidebar__items">
+    <div class="page-sidebar__item">
+      <h3 class="page-sidebar__title">人気記事</h3>
       <?php
       $args = array(
         "post_type" => "post",
@@ -19,14 +19,14 @@
       $the_query = new WP_Query($args);
       ?>
       <?php if ($the_query->have_posts()) : ?>
-        <ul class="page-blog__ranking-items">
+        <ul class="page-sidebar__ranking-items">
           <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
-            <li class="page-blog__ranking-item">
-              <article class="page-blog__ranking ranking-1st">
-                <a href="<?php the_permalink(); ?>" class="page-blog__ranking-link">
-                  <div class="page-blog__ranking-img-wrap">
-                    <div class="page-blog__ranking-img">
-                      <div class="page-blog__ranking-img">
+            <li class="page-sidebar__ranking-item">
+              <article class="page-sidebar__ranking ranking-1st">
+                <a href="<?php the_permalink(); ?>" class="page-sidebar__ranking-link">
+                  <div class="page-sidebar__ranking-img-wrap">
+                    <div class="page-sidebar__ranking-img">
+                      <div class="page-sidebar__ranking-img">
                         <?php if (has_post_thumbnail()) : ?>
                           <?php the_post_thumbnail('full', array('class' => 'img')); ?>
                         <?php else : ?>
@@ -36,9 +36,9 @@
 
                     </div>
                   </div>
-                  <div class="page-blog__wrap">
-                    <time datetime="<?php the_time('c') ?>" class="page-blog__date"><?php the_time('Y.m.d') ?></time>
-                    <p class="page-blog__ranking-title"><?php the_title(); ?></p>
+                  <div class="page-sidebar__wrap">
+                    <time datetime="<?php the_time('c') ?>" class="page-sidebar__date"><?php the_time('Y.m.d') ?></time>
+                    <p class="page-sidebar__ranking-title"><?php the_title(); ?></p>
                   </div>
                 </a>
               </article>
@@ -49,8 +49,8 @@
       <?php endif; ?>
     </div>
 
-    <div class="page-blog__sidebar-item">
-      <h3 class="page-blog__sidebar-title">口コミ</h3>
+    <div class="page-sidebar__item">
+      <h3 class="page-sidebar__title">口コミ</h3>
       <?php
       $args = array(
         "post_type" => "voice",
@@ -61,19 +61,19 @@
       $the_query = new WP_Query($args);
       ?>
       <?php if ($the_query->have_posts()) : ?>
-        <ul class="page-blog__ranking-items">
+        <ul class="page-sidebar__ranking-items">
           <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
-            <li class="page-blog__ranking-item">
-              <article class="page-blog__reviews">
-                <a class="page-blog__reviews-link" href="<?php echo esc_url(home_url('/voice')); ?>">
-                  <div class="page-blog__reviews-img">
+            <li class="page-sidebar__ranking-item">
+              <article class="page-sidebar__reviews">
+                <a class="page-sidebar__reviews-link" href="<?php echo esc_url(home_url('/voice')); ?>">
+                  <div class="page-sidebar__reviews-img">
                     <?php if (get_the_post_thumbnail()) : ?>
                       <img src="<?php the_post_thumbnail_url('full'); ?>" alt="<?php the_title(); ?>のアイキャチ画像">
                     <?php else : ?>
                       <img src="<?php echo get_theme_file_uri(); ?>/images/common/noimage.jpg" alt="noimage">
                     <?php endif; ?>
                   </div>
-                  <div class="page-blog__reviews-tag">
+                  <div class="page-sidebar__reviews-tag">
                     <?php
                     // voice-meta グループフィールドからサブフィールドの値を取得
                     $voice_meta = get_field('voice-meta');
@@ -86,9 +86,9 @@
                       <p class="voice-card__category"><?php echo $voice_age; ?>(<?php echo $voice_sex; ?>)</p>
                     </div>
                   </div>
-                  <p class="page-blog__reviews-title"><?php the_title(); ?></p>
+                  <p class="page-sidebar__reviews-title"><?php the_title(); ?></p>
                 </a>
-                <div class="page-blog__reviews-button">
+                <div class="page-sidebar__reviews-button">
                   <a href="<?php echo esc_url(home_url('/voice')); ?>" class="button"><span>view&nbsp;more</span></a>
                 </div>
               </article>
@@ -100,8 +100,8 @@
 
     </div>
 
-    <div class="page-blog__sidebar-item">
-      <h3 class="page-blog__sidebar-title">キャンペーン</h3>
+    <div class="page-sidebar__item">
+      <h3 class="page-sidebar__title">キャンペーン</h3>
       <?php
       $args = array(
         "post_type" => "campaign",
@@ -114,7 +114,7 @@
       <?php if ($the_query->have_posts()) : ?>
         <ul class="campaign-cards__items">
           <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
-            <li class="campaign-cards__item-sideber page-blog__sidebar-layout">
+            <li class="campaign-cards__item-sidebar page-sidebar-layout">
 
               <div class="campaign-card">
                 <div class="campaign-card__container">
@@ -150,24 +150,24 @@
           <?php wp_reset_postdata(); ?>
         </ul>
       <?php endif; ?>
-      <div class="page-blog__reviews-button">
+      <div class="page-sidebar__reviews-button">
         <a href="<?php echo esc_url(home_url('/campaign')); ?>" class="button"><span>view&nbsp;more</span></a>
       </div>
     </div>
 
-    <div class="page-blog__sidebar-item">
-      <h3 class="page-blog__sidebar-title">アーカイブ</h3>
-      <div class="page-blog__box js-year">
-        <div class="page-blog__sidebar-item">
+    <div class="page-sidebar__item">
+      <h3 class="page-sidebar__title">アーカイブ</h3>
+      <div class="page-sidebar__box js-year">
+        <div class="page-sidebar__item">
           <?php
           global $wpdb;
           // データベースからユニークな年を取得（2022年以降）
           $years = $wpdb->get_col("SELECT DISTINCT YEAR(post_date) FROM $wpdb->posts WHERE post_status = 'publish' AND post_type = 'post' AND YEAR(post_date) >= 2022 ORDER BY post_date DESC");
 
           foreach ($years as $year) {
-            echo '<div class="page-blog__box">';
-            echo "<div class='page-blog__year2 js-year' data-year='{$year}'>{$year}</div>";
-            echo '<ul class="page-blog__category-items hidden">';
+            echo '<div class="page-sidebar__box">';
+            echo "<div class='page-sidebar__year2 js-year' data-year='{$year}'>{$year}</div>";
+            echo '<ul class="page-sidebar__category-items hidden">';
 
             // その年の各月に投稿があるか確認
             for ($month = 1; $month <= 12; $month++) {
@@ -180,9 +180,9 @@
 
               if ($query->have_posts()) {
                 $month_name = date_i18n('F', mktime(0, 0, 0, $month, 10)); // 月の名前を取得
-                echo '<li class="page-blog__category-item">';
+                echo '<li class="page-sidebar__category-item">';
                 // 月別アーカイブへのリンク
-                echo '<a class="page-blog__category-month" href="' . get_month_link($year, $month) . '">' . $month_name . '</a>';
+                echo '<a class="page-sidebar__category-month" href="' . get_month_link($year, $month) . '">' . $month_name . '</a>';
                 echo '</li>';
               }
             }
