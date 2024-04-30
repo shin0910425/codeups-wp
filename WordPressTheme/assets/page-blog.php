7a4d0f9cb6635eb9fa98b81fa38e5cf911c010d9
shin0910425 <shin0910425@gmail.com>
2024-05-01 06:53:30 +0900

    0430　下層修正

--- WordPressTheme/assets/page-blog.php
+++ WordPressTheme/assets/page-blog.php
@@ -19,7 +19,7 @@
       <div class="page-blog__wrapper">
         <div class="page-blog__body">
           <?php if (have_posts()) : ?>
-            <ul class="page-blog__items blog-cards">
+            <ul class="page-blog__items blog-cards blog-cards--2col">
               <?php while (have_posts()) : the_post(); ?>
                 <li class="page-blog__item blog-cards__item">
                   <div class="blog-card">
